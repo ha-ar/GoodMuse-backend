21f99351ae3e7a3babb94b4f49932df0bf06f18e
Fahad <fahad@algorepublic.com>
2017-08-17 17:34:26 +0500

    errors => messages

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -43,18 +43,18 @@ class Api::V1::UsersController < ApplicationController
             else
               render :json => {
                 :success => false,
-                :errors => @user.errors.full_messages.to_sentence
+                :message => @user.errors.full_messages.to_sentence
                 }, :status => 400
               end
             else
               render :json => {
-                :error => "Role is not correct",
+                :message => "Role is not correct",
                 :success => false
                 }, :status => 400
               end
             else
               render :json => {
-                :error => "Params Incorrect",
+                :message => "Params Incorrect",
                 :success => false
                 }, :status => 400
               end
@@ -67,7 +67,7 @@ class Api::V1::UsersController < ApplicationController
               else
                 render :json => {
                   :success => false,
-                  :errors => "User was not found"
+                  :message => "User was not found"
                   }, :status => 400
                 end
               end
@@ -83,13 +83,13 @@ class Api::V1::UsersController < ApplicationController
                     else
                       render :json => {
                         :success => false,
-                        :errors => @user.errors.full_messages.to_sentence
+                        :message => @user.errors.full_messages.to_sentence
                         }, :status => 400
                       end
                     else
                       render :json => {
                         :success => false,
-                        :errors => "User was not found"
+                        :message => "User was not found"
                         }, :status => 400
                       end
                     end
@@ -103,189 +103,189 @@ class Api::V1::UsersController < ApplicationController
                           raise ActiveRecord::RecordNotFound if not @user.present?
                           password = SecureRandom.hex(4)
                           if @user.update(:password => password)
-            # UserMailer.reset_password(@user, password).deliver_now
-            render :json => {
-              :message => "Password Changed Sucessfully",
-              :new_password => password,
-              :success => true
-            }
-          else
-            render :json => {
-              :errors => "Unable to reset password",
-              :success => false
-              }, :status => 400
-            end
-          else
-            render :json => {
-              :success => false,
-              :errors => "Unable To Find User"
-              }, :status => 400
-            end
-          else
-            render :json => {
-              :success => false,
-              :error => "Params Incorrect",
-              }, :status => 400
-            end
-          end
+                                   # UserMailer.reset_password(@user, password).deliver_now
+                           render :json => {
+                              :message => "Password Changed Sucessfully",
+                              :new_password => password,
+                              :success => true
+                            }
+                          else
+                            render :json => {
+                              :message => "Unable to reset password",
+                              :success => false
+                              }, :status => 400
+                            end
+                          else
+                            render :json => {
+                              :success => false,
+                              :message => "Unable To Find User"
+                              }, :status => 400
+                            end
+                          else
+                            render :json => {
+                              :success => false,
+                              :message => "Params Incorrect",
+                              }, :status => 400
+                            end
+                          end
 
 
-          def social_login
-
-            
-            user_email = params[:user][:email]
-            user_name = params[:user][:username]
-            uid = params[:user][:social_id]
-            provider = params[:user][:social_type]
-            role = params[:user][:role]
-            phone_number = params[:user][:phone_number]
-            password = Devise.friendly_token.first(8)
+                          def social_login
 
 
-
-            if uid.present?
-              if (uid != "null" )
-
-                if user_email.present?
-                  @user = User.find_by(email: user_email)
-                end
-                unless @user.present?
-                  @user = User.find_by(uid: uid, provider: provider)
-                end
-
-                if @user.present?
-                  
-                  user_id = @user.id.to_s
-                  @user.update_attributes(:username => user_name, :uid => uid)
-
-                  if provider.present?
-                    @user.update_attributes(:provider => provider)
-                  end
-                  
-                  if phone_number.present?
-                    @user.update_attributes(:phone_number => phone_number)
-                  end
-                  
-                  if user_email.present? && !@user.email.present?
-                    @user.update_attributes(:email => user_email)
-                  end
-
-                  sign_in("user", @user)
-                  render :social_login
-
-                else
-
-                  if user_email.present?
-                    @user = User.new(:email => user_email, :uid => uid, :password => password, provider: provider)
-                  else
-                   @user = User.new(:uid => uid, provider: provider, :password => password)
-                 end
-
-                 @user.save!
-
-                 if user_name.present?
-                  @user.update_attributes(:username => user_name)
-                end
-                if phone_number.present?
-                  @user.update_attributes(:phone_number => phone_number)
-                end
-                
-                if provider.present?
-                  @user.update_attributes(:provider => provider)
-                end
-                if role.present?
-                  @user.add_role role
-                end
-                
-                @sign_up = true
-                sign_in("user", @user)
-                render :social_login
-
-              end
-            else
-              render :json => {:success => "false", :message => "User provider identity not present"}
-            end
-          else
-            render :json => {:success => "false", :message => "User provider identity not present"}
-          end
-        end
-
-
-        def user_login
-
-         if params[:user]
-
-          @user = User.find_by_email(params[:user][:email])
-
-          if @user.present?
-            if @user.valid_password?(params[:user][:password])
-              sign_in("user", @user)
-              render :user_login
-            else
-              render :json => {
-                :success => false,
-                :message => "Incorrect Email Or Password."
-                }, :status => 400
-              end
-            elsif params[:user][:username].present?
-              @user =  User.new(user_params)
-              if @user.save
-
-                if params[:user][:role] and (params[:user][:role] == 'dj' || params[:user][:role] == 'user')
-                  @user.add_role params[:user][:role]
-                end
-
-                sign_in("user", @user)
-                @sign_up = true
-                render :user_login
-              else
-                render :json => {
-                  :success => false,
-                  :errors => @user.errors.full_messages.to_sentence
-                  }, :status => 400
-                end
-              else
-               render :json => {
-                :success => false,
-                :message => "Incorrect Email Or Password."
-                }, :status => 400
-              end
-            else
-              render :json => {
-                :error => "Check Parameters!",
-                :success => false
-                }, :status => 400
-              end
-
-            end
-
-            def update_role
-
-              if params[:role]
-                @user.add_role params[:role]
-                render :json => {
-                  :message => "User Role updated sucessfully",
-                  :success => true
-                  }, :status => 400
-                else
-                  render :json => {
-                    :error => "Check Parameters!",
-                    :success => false
-                    }, :status => 400
-                  end
-                end
+                            user_email = params[:user][:email]
+                            user_name = params[:user][:username]
+                            uid = params[:user][:social_id]
+                            provider = params[:user][:social_type]
+                            role = params[:user][:role]
+                            phone_number = params[:user][:phone_number]
+                            password = Devise.friendly_token.first(8)
 
 
 
+                            if uid.present?
+                              if (uid != "null" )
 
-                private
+                                if user_email.present?
+                                  @user = User.find_by(email: user_email)
+                                end
+                                unless @user.present?
+                                  @user = User.find_by(uid: uid, provider: provider)
+                                end
 
-                def user_params
-                  params.require(:user).permit(:address, :first_name, :last_name, :username, :email, :password, :provider, :uid, :phone_number)
-                end
+                                if @user.present?
+
+                                  user_id = @user.id.to_s
+                                  @user.update_attributes(:username => user_name, :uid => uid)
+
+                                  if provider.present?
+                                    @user.update_attributes(:provider => provider)
+                                  end
+
+                                  if phone_number.present?
+                                    @user.update_attributes(:phone_number => phone_number)
+                                  end
+
+                                  if user_email.present? && !@user.email.present?
+                                    @user.update_attributes(:email => user_email)
+                                  end
+
+                                  sign_in("user", @user)
+                                  render :social_login
+
+                                else
+
+                                  if user_email.present?
+                                    @user = User.new(:email => user_email, :uid => uid, :password => password, provider: provider)
+                                  else
+                                   @user = User.new(:uid => uid, provider: provider, :password => password)
+                                 end
+
+                                 @user.save!
+
+                                 if user_name.present?
+                                  @user.update_attributes(:username => user_name)
+                                end
+                                if phone_number.present?
+                                  @user.update_attributes(:phone_number => phone_number)
+                                end
+
+                                if provider.present?
+                                  @user.update_attributes(:provider => provider)
+                                end
+                                if role.present?
+                                  @user.add_role role
+                                end
+
+                                @sign_up = true
+                                sign_in("user", @user)
+                                render :social_login
+
+                              end
+                            else
+                              render :json => {:success => "false", :message => "User provider identity not present"}
+                            end
+                          else
+                            render :json => {:success => "false", :message => "User provider identity not present"}
+                          end
+                        end
 
 
-                def get_user 
-                  @user = User.find_by_id(params[:id])
-                end
-              end
+                        def user_login
+
+                         if params[:user]
+
+                          @user = User.find_by_email(params[:user][:email])
+
+                          if @user.present?
+                            if @user.valid_password?(params[:user][:password])
+                              sign_in("user", @user)
+                              render :user_login
+                            else
+                              render :json => {
+                                :success => false,
+                                :message => "Incorrect Email Or Password."
+                                }, :status => 400
+                              end
+                            elsif params[:user][:username].present?
+                              @user =  User.new(user_params)
+                              if @user.save
+
+                                if params[:user][:role] and (params[:user][:role] == 'dj' || params[:user][:role] == 'user')
+                                  @user.add_role params[:user][:role]
+                                end
+
+                                sign_in("user", @user)
+                                @sign_up = true
+                                render :user_login
+                              else
+                                render :json => {
+                                  :success => false,
+                                  :message => @user.errors.full_messages.to_sentence
+                                  }, :status => 400
+                                end
+                              else
+                               render :json => {
+                                :success => false,
+                                :message => "Incorrect Email Or Password."
+                                }, :status => 400
+                              end
+                            else
+                              render :json => {
+                                :message => "Check Parameters!",
+                                :success => false
+                                }, :status => 400
+                              end
+
+                            end
+
+                            def update_role
+
+                              if params[:role]
+                                @user.add_role params[:role]
+                                render :json => {
+                                  :message => "User Role updated sucessfully",
+                                  :success => true
+                                  }, :status => 400
+                                else
+                                  render :json => {
+                                    :message => "Check Parameters!",
+                                    :success => false
+                                    }, :status => 400
+                                  end
+                                end
+
+
+
+
+                                private
+
+                                def user_params
+                                  params.require(:user).permit(:address, :first_name, :last_name, :username, :email, :password, :provider, :uid, :phone_number)
+                                end
+
+
+                                def get_user 
+                                  @user = User.find_by_id(params[:id])
+                                end
+                              end
