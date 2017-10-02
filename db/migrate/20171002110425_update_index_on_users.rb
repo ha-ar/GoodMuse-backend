1618e69809bba85b750fcda80fa5b65314421941
Fahad <fahad@algorepublic.com>
2017-10-02 17:18:06 +0500

    ..

--- db/migrate/20171002110425_update_index_on_users.rb
+++
@@ -1,7 +0,0 @@
-class UpdateIndexOnUsers < ActiveRecord::Migration[5.0]
-	def up
-		sql = 'DROP INDEX index_users_on_email'
-		sql << ' ON users' if Rails.env == 'production'
-		ActiveRecord::Base.connection.execute(sql)
-	end
-end
